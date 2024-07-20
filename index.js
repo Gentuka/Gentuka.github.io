57586dcf4d546797c357a90720e10eae68dbc579
Gentuka <gent.ntm@gmail.com>
2024-07-20 23:27:50 +0200

    darkmode and UI Improvement, use breakstart and breakEnd instead of break value, add new break Values

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 let startTime = document.querySelector('#startTime');
 let workTime = document.querySelector('#worktime');
-let breakDuration = document.querySelector('#breakDuration');
+let breakStart = document.querySelector('#breakStart');
+let breakEnd = document.querySelector('#breakEnd');
 let overtime = document.querySelector('#overtime');
 let endTime = document.querySelector('#endTime');
+
+let addBreak= document.querySelector('#add-break');
 
 // I HATE JAVASCRIPT
 
@@ -33,13 +36,42 @@ function calcEndTime(time1, time2, breakDuration, overtime) {
     return minutesToTimeString(totalMinutes);
 }
 
-function updateEndTime() {
-    endTime.value = calcEndTime(startTime, workTime, breakDuration.value, overtime.value);
+function calcBreakDuration(breakStart, breakEnd) {
+    return timeToMinute(breakEnd) - timeToMinute(breakStart)
 }
 
-endTime.value = calcEndTime(startTime, workTime, breakDuration.value, overtime.value);
+function updateEndTime() {
+    let breakDuration = calcBreakDuration(breakStart, breakEnd)
+    endTime.innerHTML= calcEndTime(startTime, workTime, breakDuration, overtime.value);
+}
+
+function addBreakElements() {
+    alert('in progress');
+    var breakStartDiv = document.createElement('div');
+    breakStartDiv.className = 'col form-group mb-4';
+    breakStartDiv.innerHTML = `
+                <label for="breakStart">Break Start*</label>
+                <input type="time" class="form-control" id="breakStart" name="worktime" value="12:00" required>
+            `;
+
+    var breakEndDiv = document.createElement('div');
+    breakEndDiv.className = 'col form-group mb-4';
+    breakEndDiv.innerHTML = `
+                <label for="breakEnd">Break End*</label>
+                <input type="time" class="form-control" id="breakEnd" name="worktime" value="12:30" required>
+            `;
+
+    var container = document.getElementById('break-holder');
+    container.appendChild(breakStartDiv);
+    container.appendChild(breakEndDiv);
+}
+
+addBreak.addEventListener('click', addBreakElements)
+
+updateEndTime()
 startTime.addEventListener('input', updateEndTime);
 workTime.addEventListener('input', updateEndTime);
-breakDuration.addEventListener('input', updateEndTime);
+breakStart.addEventListener('input', updateEndTime);
+breakEnd.addEventListener('input', updateEndTime);
 overtime.addEventListener('input', updateEndTime);
 
